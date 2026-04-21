611078f5cd977856765fe8ceb0bc71b2533c5008
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-04-21 13:47:38 +0530

    updated

--- middleware/authmiddleware.js
+++ middleware/authmiddleware.js
@@ -3,62 +3,49 @@ const User = require("../models/User");
 
 
 
-// protect rooutes jwt
+// protect routes jwt
 
 const protect= async (req,res,next)=>{
-    let token;
-    if(
-        req.headers.authorization &&  // check if authorization header exists
-        req.headers.authorization.startsWith("Bearer")){      //insure its start with bearer
-
-        try{
-          token= req.headers.authorization.split(" ")[1];  //extract the actual token after bearer
-          const decode= jwt.verify(token,process.env.JWT_SECRET);  //verify jwt token
-          req.user= await User.findById(decode.id).select("-password"); // find the user by id exclude password
-          next(); // Pass control to next middleware/route
-        }catch(error){
-            return res
-            .status(401) // unauthorized status
-            .json({message:"❌ Not authorized,invalid token"}); // error message invalid token
-
-        }
-
+  let token;
+  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decode = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decode.id).select("-password");
+      next();
+    } catch (error) {
+      return res.status(401).json({ message: "❌ Not authorized, invalid token" });
     }
-   if(!token){
-    return res.status(401).json({message:"❌ no authorized,no token"}); //error not token provided
-   }
-
+  } else {
+    return res.status(401).json({ message: "❌ No authorized, no token" });
+  }
 };
    // admin only 
    const adminonly= (req,res,next) =>{
-      if(req.user && req.user.role==="admin"){  // check if the admin exiest
+      if(req.user && req.user.role==="admin"){  // check if the admin exists
           next(); //allow access
       }else{
-        return res.status(403).json({message:"❌ access deinded, admin only"}) // Forbidden if not admin
+        return res.status(403).json({message:"❌ access denied, admin only"}); // Forbidden if not admin
       }
    };
 
 
    //Recruiter
    const recruiter = (req,res,next)=>{
-    if(req.user&& req.user.role==="recruiter"){  //check if the recruiter exiest
+    if(req.user&& req.user.role==="recruiter"){  //check if the recruiter exists
         next();//allow access
     }else{
-        return res.status(403).json({message:"❌ access deinded,recruiter only"}); //forbidden if not recruiter
+        return res.status(403).json({message:"❌ access denied,recruiter only"}); //forbidden if not recruiter
     }
    };
 
+   //Recruiter or Admin
+   const recruiterOrAdmin = (req, res, next) => {
+     if (req.user && (req.user.role === "recruiter" || req.user.role === "admin")) {
+       next();
+     } else {
+       return res.status(403).json({ message: "❌ access denied, recruiter or admin only" });
+     }
+   };
 
-// recruiterORadmin
-
-const recruiterORadmin=(req,res,next)=>{
-    if(req.user&& //ensure user exiest
-        (req.user.role==="recruiter" || req.user.role==="admin")
-    ){
-        next(); //allow access
-    }else{
-      return res.status(403).json({message:"❌ access deinded,admin and recruiter only"})
-    }
-};
-
-module.exports={protect,adminonly,recruiter,recruiterORadmin};
+module.exports={protect,adminonly,recruiter,recruiterOrAdmin};
