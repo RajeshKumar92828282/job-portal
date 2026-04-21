6de6d27597853fb14210bd38cf95cfac9da85dd3
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-04-21 13:46:55 +0530

    updated

--- controllers/authcontrol.js
+++ controllers/authcontrol.js
@@ -1,6 +1,5 @@
 const jwt=require("jsonwebtoken");
 const User= require("../models/User");
-const { model } = require("mongoose");
 
 //generate token
 const generatetoken=(id)=>{
@@ -18,19 +17,19 @@ const registerUser = async (req,res)=>{
         const {name,email,password,role} = req.body;
 
         if(!name||!email || !password){
-            return res.status(400).json({messgae:"please fill all required"});
+            return res.status(400).json({message:"please fill all required"});
         }
 
-          //check exiest user
-          const exiestUser= await User.findOne({email});
-          if(exiestUser){
-            return res.status(400).json({messgae:"alarady user exiest"});
+          //check exist user
+          const existUser= await User.findOne({email});
+          if(existUser){
+            return res.status(400).json({message:"already user exist"});
           }
           //create a new user
           const user= await User.create({name,email,password,role});
 
           res.status(201).json({
-              messgae:"User register successfully",
+              message:"User register successfully",
               token:generatetoken(user._id),
               user:{
                 id:user._id,
@@ -89,7 +88,7 @@ try{
     res.status(200).json({message:"✅ profile fetched successfully",user});
 
 }catch(error){
-    res.status(500).json({message:"servr error",error:error.message});
+    res.status(500).json({message:"server error",error:error.message});
 }
 
 
