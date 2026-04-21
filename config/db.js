e4c2305fcd9d6d83834bddc28ca6cc24038b6177
Rajesh Kumar Behera <rb3302436@gmail.com>
2026-04-21 13:45:57 +0530

    updated

--- config/db.js
+++ config/db.js
@@ -1,8 +1,8 @@
-const monngose= require("mongoose");
+const mongoose= require("mongoose");
 
 const mongoDB= async () => {
   try{
-      const connect= await monngose.connect(process.env.MONGO_URI);
+      const connect= await mongoose.connect(process.env.MONGO_URI);
       console.log(`✅ mongodb connect: ${connect.connection.host}`);
   }catch(error){
     console.error(`❌ error: ${error.message}`);
